43355b60a246307b404104a84d32eaccc6d7fad2
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-03-09 18:01:25 +0100

    Fix no multiplier extension cutting of last digit & regex fix

--- bot/util.py
+++ bot/util.py
@@ -2,16 +2,15 @@ import re
 
 
 def validate_string_format(s: str) -> bool:
-    pattern = r"(\d+(?:[\.\d]*)?[,\d]+)([KMGTPE]?)"
+    pattern = r"(\d+(?:[\.\d]*)?[,\d]+)([KMBT]?)"
     return bool(re.match(pattern, s))
 
 
-def convert_string_to_int(s: str) -> int:
-    s = s.strip().lower().replace(",", ".")
-    multiplier = 1_000 if s.endswith("k") else (1_000_000 if s.endswith("m") else (
-        1_000_000_000 if s.endswith("b") else (
-            1_000_000_000_000 if s.endswith("t") else 1)
+def convert_string_to_int(number: str) -> int:
+    number = number.strip().lower().replace(",", ".")
+    multiplier = 1_000 if number.endswith("k") else (1_000_000 if number.endswith("m") else (
+        1_000_000_000 if number.endswith("b") else (
+            1_000_000_000_000 if number.endswith("t") else 1)
         )
-    )
-    result = float(s[:-1]) * multiplier
-    return int(result)
+                                                     )
+    return int(number) if multiplier == 1 else int(float(number[:-1]) * multiplier)
