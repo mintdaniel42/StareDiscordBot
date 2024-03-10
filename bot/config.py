a4f73157f95fced670a4518959e47be3d51d5a0e
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-03-10 09:32:20 +0100

    Remove unused config field

--- bot/config.py
+++ bot/config.py
@@ -7,5 +7,4 @@ CREATE_ROLE_ID = os.getenv('CREATE_ROLE_ID')
 VIEW_ROLE_ID = os.getenv('VIEW_ROLE_ID')
 EPHEMERAL = True if os.getenv('EPHEMERAL') == 1 else 0
 ENTRIES_PER_PAGE = int(os.getenv('ENTRIES_PER_PAGE'))
-HIGHSCORE_ICON = os.getenv('HIGHSCORE_ICON')
 COLOR = int(os.getenv('COLOR'), 16)
