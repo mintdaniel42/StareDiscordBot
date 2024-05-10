d1e8db94f373a8ee023b608e208553a7aa551413
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-10 16:06:33 +0200

    Allow RequestModel to be created from a HNSUserModel or a PGUserModel

--- src/main/java/org/mintdaniel42/starediscordbot/db/RequestModel.java
+++ src/main/java/org/mintdaniel42/starediscordbot/db/RequestModel.java
@@ -2,13 +2,12 @@ package org.mintdaniel42.starediscordbot.db;
 
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Value;
+import lombok.*;
 
 import java.util.UUID;
 
 @Value
+@Builder(toBuilder = true, access = AccessLevel.PRIVATE)
 @DatabaseTable(tableName = "requests")
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
@@ -25,6 +24,32 @@ public class RequestModel {
     @DatabaseField boolean banned;
     @DatabaseField boolean cheating;
     @DatabaseField Database database;
+
+    public static @NonNull RequestModel from(long timestamp, @NonNull HNSUserModel hnsUserModel) {
+        return RequestModel.builder()
+                .timestamp(timestamp)
+                .uuid(hnsUserModel.getUuid())
+                .rating(hnsUserModel.getRating())
+                .joined(hnsUserModel.getJoined())
+                .points(hnsUserModel.getPoints())
+                .secondary(hnsUserModel.isSecondary())
+                .banned(hnsUserModel.isBanned())
+                .cheating(hnsUserModel.isCheating())
+                .build();
+    }
+
+    public static @NonNull RequestModel from(long timestamp, @NonNull PGUserModel pgUserModel) {
+        return RequestModel.builder()
+                .timestamp(timestamp)
+                .uuid(pgUserModel.getUuid())
+                .rating(pgUserModel.getRating())
+                .joined(pgUserModel.getJoined())
+                .points(pgUserModel.getPoints())
+                .luck(pgUserModel.getLuck())
+                .quota(pgUserModel.getQuota())
+                .winrate(pgUserModel.getWinrate())
+                .build();
+    }
 
     public enum Database {
         PG,
