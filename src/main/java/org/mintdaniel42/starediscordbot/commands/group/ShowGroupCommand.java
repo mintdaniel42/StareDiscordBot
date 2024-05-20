64d738736665aeb97a9cafe621d387992062e091
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-20 11:51:46 +0200

    Make ShowGroupCommand actually show the group

--- src/main/java/org/mintdaniel42/starediscordbot/commands/group/ShowGroupCommand.java
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/group/ShowGroupCommand.java
@@ -2,8 +2,10 @@ package org.mintdaniel42.starediscordbot.commands.group;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.mintdaniel42.starediscordbot.db.DatabaseAdapter;
 import org.mintdaniel42.starediscordbot.db.GroupModel;
 import org.mintdaniel42.starediscordbot.embeds.GroupEmbed;
@@ -15,6 +17,28 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public final class ShowGroupCommand extends ListenerAdapter {
 	@NonNull private final DatabaseAdapter databaseAdapter;
+
+	@Override
+	public void onSlashCommandInteraction(@NonNull final SlashCommandInteractionEvent event) {
+		if (!event.getFullCommandName().equals("group show")) return;
+
+		// check maintenance
+		if (Options.isInMaintenance()) {
+			event.reply(R.string("the_bot_is_currently_in_maintenance_mode")).queue();
+			return;
+		}
+
+		// check if group exists and send if yes
+		GroupModel groupModel;
+		OptionMapping tagMapping = event.getOption("tag");
+		if (tagMapping == null) {
+			event.reply(R.string("your_command_was_incomplete")).queue();
+		} else if ((groupModel = databaseAdapter.getGroup(tagMapping.getAsString())) == null) {
+			event.reply(R.string("this_group_does_not_exist")).queue();
+		} else {
+			event.replyEmbeds(GroupEmbed.of(databaseAdapter, groupModel)).queue();
+		}
+	}
 
 	@Override
 	public void onButtonInteraction(@NonNull final ButtonInteractionEvent event) {
