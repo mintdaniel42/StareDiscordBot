6b2767d9ecb6d4b840da5e51dbab9385243ecdee
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-20 21:31:12 +0200

    Make way better UserEmbed

--- src/main/java/org/mintdaniel42/starediscordbot/embeds/UserEmbed.java
+++ src/main/java/org/mintdaniel42/starediscordbot/embeds/UserEmbed.java
@@ -5,9 +5,7 @@ import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.jetbrains.annotations.Contract;
-import org.mintdaniel42.starediscordbot.db.DatabaseAdapter;
-import org.mintdaniel42.starediscordbot.db.HNSUserModel;
-import org.mintdaniel42.starediscordbot.db.PGUserModel;
+import org.mintdaniel42.starediscordbot.db.*;
 import org.mintdaniel42.starediscordbot.utils.MCHelper;
 import org.mintdaniel42.starediscordbot.utils.Options;
 import org.mintdaniel42.starediscordbot.utils.R;
@@ -15,39 +13,45 @@ import org.mintdaniel42.starediscordbot.utils.R;
 @UtilityClass
 public class UserEmbed {
     @Contract(pure = true, value = "_, _ -> new")
-    public @NonNull MessageEmbed of(@NonNull DatabaseAdapter databaseAdapter, @NonNull HNSUserModel hnsUserModel) {
-        EmbedBuilder embedBuilder = new EmbedBuilder();
+    public @NonNull MessageEmbed of(@NonNull final UserModel userModel, @NonNull final Type type) {
+        EmbedBuilder builder = new EmbedBuilder();
 
-        embedBuilder.setTitle(R.string("hide_n_seek_player_database"));
-        embedBuilder.setDescription(MCHelper.getUsername(databaseAdapter, hnsUserModel.getUuid()));
-        embedBuilder.setColor(Options.getColorNormal());
-        embedBuilder.setThumbnail(MCHelper.getThumbnail(hnsUserModel.getUuid()));
-        embedBuilder.addField(R.string("rating"), hnsUserModel.getRating(), true);
-        embedBuilder.addField(R.string("points"), formatNumber(hnsUserModel.getPoints()), true);
-        embedBuilder.addField(R.string("joined"), hnsUserModel.getJoined(), true);
-        embedBuilder.addField(R.string("secondary"), hnsUserModel.isSecondary() ? "✅" : "❌", true);
-        embedBuilder.addField(R.string("banned"), hnsUserModel.isBanned() ? "✅" : "❌", true);
-        embedBuilder.addField(R.string("cheating"), hnsUserModel.isCheating() ? "✅" : "❌", true);
+        builder.setDescription(userModel.getUsername());
+        builder.setColor(Options.getColorNormal());
+        builder.setThumbnail(MCHelper.getThumbnail(userModel.getUuid()));
 
-        return embedBuilder.build();
-    }
+        switch (type) {
+            case HNS -> {
+                builder.setTitle(R.string("hide_n_seek_player_database"));
+                builder.addField(R.string("rating"), userModel.getHnsUser().getRating(), true);
+                builder.addField(R.string("points"), formatNumber(userModel.getHnsUser().getPoints()), true);
+                builder.addField(R.string("joined"), userModel.getHnsUser().getJoined(), true);
+                builder.addField(R.string("note"), userModel.getNote(), userModel.getNote().length() <= 10);
+                builder.addField(R.string("banned"), userModel.getHnsUser().isBanned() ? "✅" : "❌", true);
+                builder.addField(R.string("cheating"), userModel.getHnsUser().isCheating() ? "✅" : "❌", true);
+            }
+            case HNS_MORE -> {
+                builder.setTitle(R.string("hide_n_seek_player_database_more_information"));
+                builder.addField(R.string("top10"), userModel.getHnsUser().getTop10(), true);
+                builder.addField(R.string("streak"), formatNumber(userModel.getHnsUser().getStreak()), true);
+                builder.addField(R.string("highest_rank"), userModel.getHnsUser().getHighestRank(), true);
+                builder.addField(R.string("secondary"), userModel.getHnsUser().isSecondary() ? "✅" : "❌", true);
+                builder.addField(R.string("discord_tag"), userModel.getDiscord() == 0 ? "❌" : "<@%s>".formatted(userModel.getDiscord()), true);
+                builder.addField(R.string("group_name"), userModel.getGroup() != null ? userModel.getGroup().getName() : "❌", true);
+            }
+            case PG -> {
+                builder.setTitle(R.string("partygames_player_database"));
+                builder.setThumbnail(MCHelper.getThumbnail(userModel.getPgUser().getUuid()));
+                builder.addField(R.string("rating"), userModel.getPgUser().getRating(), true);
+                builder.addField(R.string("points"), formatNumber(userModel.getPgUser().getPoints()), true);
+                builder.addField(R.string("joined"), userModel.getPgUser().getJoined(), true);
+                builder.addField(R.string("luck"), String.valueOf(userModel.getPgUser().getLuck()), true);
+                builder.addField(R.string("quota"), String.format("%s%%", userModel.getPgUser().getQuota()), true);
+                builder.addField(R.string("winrate"), String.format("%s%%", userModel.getPgUser().getWinrate()), true);
+            }
+        }
 
-    @Contract(pure = true, value = "_, _ -> new")
-    public @NonNull MessageEmbed of(@NonNull DatabaseAdapter databaseAdapter, @NonNull PGUserModel pgUserModel) {
-        EmbedBuilder embedBuilder = new EmbedBuilder();
-
-        embedBuilder.setTitle(R.string("partygames_player_database"));
-        embedBuilder.setDescription(MCHelper.getUsername(databaseAdapter, pgUserModel.getUuid()));
-        embedBuilder.setColor(Options.getColorNormal());
-        embedBuilder.setThumbnail(MCHelper.getThumbnail(pgUserModel.getUuid()));
-        embedBuilder.addField(R.string("rating"), pgUserModel.getRating(), true);
-        embedBuilder.addField(R.string("points"), formatNumber(pgUserModel.getPoints()), true);
-        embedBuilder.addField(R.string("joined"), pgUserModel.getJoined(), true);
-        embedBuilder.addField(R.string("luck"), String.valueOf(pgUserModel.getLuck()), true);
-        embedBuilder.addField(R.string("quota"), String.format("%s%%", pgUserModel.getQuota()), true);
-        embedBuilder.addField(R.string("winrate"), String.format("%s%%", pgUserModel.getWinrate()), true);
-
-        return embedBuilder.build();
+        return builder.build();
     }
 
     @Contract(pure = true, value = "_ -> new")
@@ -56,5 +60,11 @@ public class UserEmbed {
         else if (value >= 5_000_000L) return Math.round(value / 1_000_000) + "M";
         else if (value >= 5_000) return Math.round(value / 1_000) + "K";
         else return String.valueOf(value);
+    }
+
+    public enum Type {
+        HNS,
+        HNS_MORE,
+        PG
     }
 }
