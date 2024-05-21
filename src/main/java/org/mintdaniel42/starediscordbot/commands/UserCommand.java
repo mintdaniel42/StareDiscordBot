e10bff1a8e9e73c8e5021a752aadfb5518bfa8af
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-21 20:31:57 +0200

    Add user edit functionality

--- src/main/java/org/mintdaniel42/starediscordbot/commands/UserCommand.java
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/UserCommand.java
@@ -2,10 +2,15 @@ package org.mintdaniel42.starediscordbot.commands;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.mintdaniel42.starediscordbot.db.DatabaseAdapter;
+import org.mintdaniel42.starediscordbot.db.RequestModel;
 import org.mintdaniel42.starediscordbot.db.UserModel;
 import org.mintdaniel42.starediscordbot.embeds.UserEmbed;
 import org.mintdaniel42.starediscordbot.utils.DCHelper;
@@ -58,7 +63,42 @@ public class UserCommand extends ListenerAdapter {
 	}
 
 	private void userEdit(@NonNull final SlashCommandInteractionEvent event) {
+		if (event.getOption("username") instanceof OptionMapping usernameMapping && event.getOptions().size() >= 2) {
+			if (MCHelper.getUuid(databaseAdapter, usernameMapping.getAsString()) instanceof UUID uuid) {
+				if (databaseAdapter.getUser(uuid) instanceof UserModel userModel) {
+					UserModel.UserModelBuilder userBuilder = userModel.toBuilder();
 
+					for (OptionMapping optionMapping : event.getOptions()) {
+						switch (optionMapping.getName()) {
+							case "discord" -> userBuilder.discord(optionMapping.getAsLong());
+							case "note" -> userBuilder.note(optionMapping.getAsString());
+						}
+					}
+
+					if (!DCHelper.hasRole(event.getMember(), Options.getEditRoleId()) && !DCHelper.hasRole(event.getMember(), Options.getCreateRoleId())) {
+						long timestamp = System.currentTimeMillis();
+						if (event.getGuild() instanceof Guild guild) {
+							if (guild.getTextChannelById(Options.getRequestChannelId()) instanceof TextChannel requestChannel) {
+								if (event.getMember() instanceof Member member) {
+									if (databaseAdapter.addRequest(RequestModel.from(timestamp, userModel))) {
+										requestChannel.sendMessage(R.string("the_user_s_requested_an_edit_you_can_approve_it_with_approve_s",
+														member.getAsMention(),
+														timestamp))
+												.addActionRow(Button.primary(String.format("approve:%s", timestamp), R.string("approve_this_change")))
+												.addEmbeds(UserEmbed.of(userModel, UserEmbed.Type.HNS_ALL)).queue();
+										event.reply(R.string("the_entry_change_was_successfully_requested")).queue();
+									} else event.reply(R.string("the_entry_could_not_be_updated")).queue();
+								}
+							}
+						}
+					} else if (databaseAdapter.editHnsUser(userModel.getHnsUser()) == 0 || databaseAdapter.editUser(userModel) == 0) {
+						event.reply(R.string("the_entry_could_not_be_updated")).queue();
+					} else event.reply(R.string("the_entry_was_successfully_updated"))
+							.setEmbeds(UserEmbed.of(userModel, UserEmbed.Type.HNS))
+							.queue();
+				} else event.reply(R.string("this_user_entry_does_not_exist")).queue();
+			} else event.reply(R.string("this_username_does_not_exist")).queue();
+		} else event.reply(R.string("your_command_was_incomplete")).queue();
 	}
 
 	private void userDelete(@NonNull final SlashCommandInteractionEvent event) {
