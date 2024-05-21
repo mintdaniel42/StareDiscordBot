693480cad377076a75397acdbe88955afea2733b
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-21 13:09:49 +0200

    Restructure ApproveChangeCommand

--- src/main/java/org/mintdaniel42/starediscordbot/commands/ApproveChangeCommand.java
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/ApproveChangeCommand.java
@@ -17,52 +17,30 @@ public final class ApproveChangeCommand extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(@NonNull final SlashCommandInteractionEvent event) {
-        if (!event.getFullCommandName().equals("approve")) return;
-
-        // check maintenance
-        if (Options.isInMaintenance()) {
-            event.reply(R.string("the_bot_is_currently_in_maintenance_mode")).queue();
-            return;
-        }
-
-        // check permission level
-        if (DCHelper.lacksRole(event.getMember(), Options.getEditRoleId()) && DCHelper.lacksRole(event.getMember(), Options.getCreateRoleId())) {
-            event.reply(R.string("you_do_not_have_the_permission_to_use_this_command")).queue();
-            return;
-        }
-
-        // try to merge the change
-        OptionMapping idMapping = event.getOption("id");
-        if (!(idMapping == null) && databaseAdapter.mergeRequest(idMapping.getAsLong())) {
-            event.reply(R.string("request_was_successfully_merged")).queue();
-        } else {
-            event.reply(R.string("request_could_not_be_merged")).queue();
+        if (event.getFullCommandName().equals("approve")) {
+            if (!Options.isInMaintenance()) {
+                if (DCHelper.hasRole(event.getMember(), Options.getEditRoleId()) || DCHelper.hasRole(event.getMember(), Options.getCreateRoleId())) {
+                    if (event.getOption("id") instanceof OptionMapping idMapping) {
+                        if (databaseAdapter.mergeRequest(idMapping.getAsLong())) {
+                            event.reply(R.string("request_was_successfully_merged")).queue();
+                        } else event.reply(R.string("request_could_not_be_merged")).queue();
+                    } else event.reply(R.string("your_command_was_incomplete")).queue();
+                } else event.reply(R.string("you_do_not_have_the_permission_to_use_this_command")).queue();
+            } else event.reply(R.string("the_bot_is_currently_in_maintenance_mode")).queue();
         }
     }
 
     @Override
     public void onButtonInteraction(@NonNull final ButtonInteractionEvent event) {
         String[] buttonParts = event.getComponentId().split(":");
-        if (!buttonParts[0].equals("approve") || buttonParts.length != 2) return;
-
-        // check maintenance
-        if (Options.isInMaintenance()) {
-            event.reply(R.string("the_bot_is_currently_in_maintenance_mode")).queue();
-            return;
-        }
-
-        // check permission level
-        if (DCHelper.lacksRole(event.getMember(), Options.getEditRoleId()) && DCHelper.lacksRole(event.getMember(), Options.getCreateRoleId())) {
-            event.reply(R.string("you_do_not_have_the_permission_to_use_this_button")).queue();
-            return;
-        }
-
-        // try to merge the change
-        long id = Long.parseLong(buttonParts[1]);
-        if ((id != -1) && databaseAdapter.mergeRequest(id)) {
-            event.reply(R.string("request_was_successfully_merged")).queue();
-        } else {
-            event.reply(R.string("request_could_not_be_merged")).queue();
+        if (buttonParts[0].equals("approve") && buttonParts.length == 2) {
+            if (!Options.isInMaintenance()) {
+                if (DCHelper.hasRole(event.getMember(), Options.getEditRoleId()) || DCHelper.hasRole(event.getMember(), Options.getCreateRoleId())) {
+                    if (databaseAdapter.mergeRequest(Long.parseLong(buttonParts[1]))) {
+                        event.reply(R.string("request_was_successfully_merged")).queue();
+                    } else event.reply(R.string("request_could_not_be_merged")).queue();
+                } else event.reply(R.string("you_do_not_have_the_permission_to_use_this_button")).queue();
+            } else event.reply(R.string("the_bot_is_currently_in_maintenance_mode")).queue();
         }
     }
 }
