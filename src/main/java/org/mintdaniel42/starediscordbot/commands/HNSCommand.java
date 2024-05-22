b25466a1f7c8e7702edd7fd09f5dbb3c4ace6f21
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-22 08:05:38 +0200

    Extract method for hns model creation

--- src/main/java/org/mintdaniel42/starediscordbot/commands/HNSCommand.java
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/HNSCommand.java
@@ -63,18 +63,16 @@ public final class HNSCommand extends ListenerAdapter {
 							).withDisabled(!Features.dev),
 							Button.primary(
 									String.format("group:%s", groupModel != null ? groupModel.getTag() : null),
-									R.string("show_group")).withDisabled(!databaseAdapter.hasGroupFor(uuid) || groupModel == null)
+									R.string("show_group")).withDisabled(!databaseAdapter.hasGroupFor(uuid))
 							).withDisabled(!Features.dev)
 					).queue());
-		} else {
-			event.reply(R.string("this_username_or_entry_does_not_exist")).queue();
-		}
+		} else event.reply(R.string("this_username_or_entry_does_not_exist")).queue();
 	}
 
 	private void hnsShow(@NonNull final SlashCommandInteractionEvent event, final boolean more) {
-		if (event.getOption("username") instanceof OptionMapping usernameMapping) {
-			if (MCHelper.getUuid(databaseAdapter, usernameMapping.getAsString()) instanceof UUID uuid) {
-				if (databaseAdapter.getUser(uuid) instanceof UserModel userModel) {
+		if (event.getOption("username") instanceof final OptionMapping usernameMapping) {
+			if (MCHelper.getUuid(databaseAdapter, usernameMapping.getAsString()) instanceof final UUID uuid) {
+				if (databaseAdapter.hasHnsUser(uuid) && databaseAdapter.getUser(uuid) instanceof final UserModel userModel) {
 					event.deferReply().queue(interactionHook -> interactionHook
 							.editOriginalEmbeds(UserEmbed.of(userModel, more ? UserEmbed.Type.HNS_MORE : UserEmbed.Type.HNS))
 							.setComponents(ActionRow.of(
@@ -94,35 +92,15 @@ public final class HNSCommand extends ListenerAdapter {
 
 	private void hnsAdd(@NonNull final SlashCommandInteractionEvent event) {
 		if (DCHelper.hasRole(event.getMember(), Options.getCreateRoleId())) {
-			if (event.getOption("username") instanceof OptionMapping usernameMapping && event.getOptions().size() >= 2) {
-				if (MCHelper.getUuid(databaseAdapter, usernameMapping.getAsString()) instanceof UUID uuid) {
+			if (event.getOption("username") instanceof final OptionMapping usernameMapping && event.getOptions().size() >= 2) {
+				if (MCHelper.getUuid(databaseAdapter, usernameMapping.getAsString()) instanceof final UUID uuid) {
 					if (!databaseAdapter.hasHnsUser(uuid)) {
-						// get builder
-						HNSUserModel.HNSUserModelBuilder hnsBuilder;
-						UserModel.UserModelBuilder userBuilder;
-						hnsBuilder = HNSUserModel.builder().uuid(uuid);
-						UserModel userModel = databaseAdapter.getUser(uuid);
-						userBuilder = userModel == null ? UserModel.builder().uuid(uuid) : userModel.toBuilder();
+						final var hnsModel = buildHnsModel(event, HNSUserModel.builder().uuid(uuid));
+						final var userModel = databaseAdapter.getUser(uuid);
+						final var userBuilder = userModel == null ? UserModel.builder().uuid(uuid) : userModel.toBuilder();
+						userBuilder.hnsUser(hnsModel).build();
 
-						// set attributes
-						for (OptionMapping optionMapping : event.getOptions()) {
-							switch (optionMapping.getName()) {
-								case "rating" -> hnsBuilder.rating(optionMapping.getAsString());
-								case "points" -> hnsBuilder.points(Math.round(optionMapping.getAsDouble()));
-								case "joined" -> hnsBuilder.joined(optionMapping.getAsString());
-								case "secondary" -> hnsBuilder.secondary(optionMapping.getAsBoolean());
-								case "banned" -> hnsBuilder.banned(optionMapping.getAsBoolean());
-								case "cheating" -> hnsBuilder.cheating(optionMapping.getAsBoolean());
-								case "top10" -> hnsBuilder.top10(optionMapping.getAsString());
-								case "streak" -> hnsBuilder.streak(optionMapping.getAsInt());
-								case "highest_rank" -> hnsBuilder.highestRank(optionMapping.getAsString());
-							}
-						}
-
-						// update the model
-						userBuilder.hnsUser(hnsBuilder.build()).build();
-
-						if (!databaseAdapter.addUser(userBuilder.build()) && !databaseAdapter.addHnsUser(hnsBuilder.build())) {
+						if (!databaseAdapter.addUser(userBuilder.build()) && !databaseAdapter.addHnsUser(hnsModel)) {
 							event.reply(R.string("the_entry_could_not_be_created")).queue();
 						} else event.reply(R.string("the_entry_was_successfully_created"))
 								.setEmbeds(UserEmbed.of(userBuilder.build(), UserEmbed.Type.HNS_ALL))
@@ -134,13 +112,12 @@ public final class HNSCommand extends ListenerAdapter {
 	}
 
 	private void hnsEdit(@NonNull final SlashCommandInteractionEvent event) {
-		if (event.getOption("username") instanceof OptionMapping usernameMapping && event.getOptions().size() >= 2) {
-			if (MCHelper.getUuid(databaseAdapter, usernameMapping.getAsString()) instanceof UUID uuid) {
-				if (databaseAdapter.getHnsUser(uuid) instanceof HNSUserModel hnsUserModel && 
+		if (event.getOption("username") instanceof final OptionMapping usernameMapping &&
+				event.getOptions().size() >= 2) {
+			if (MCHelper.getUuid(databaseAdapter, usernameMapping.getAsString()) instanceof final UUID uuid) {
+				if (databaseAdapter.getHnsUser(uuid) instanceof HNSUserModel hnsUserModel &&
 						databaseAdapter.getUser(uuid) instanceof UserModel userModel) {
-					HNSUserModel.HNSUserModelBuilder hnsBuilder = getHnsBuilder(event, hnsUserModel);
-
-					hnsUserModel = hnsBuilder.build();
+					hnsUserModel = buildHnsModel(event, hnsUserModel.toBuilder());
 					userModel = userModel.toBuilder().hnsUser(hnsUserModel).build();
 
 					if (!DCHelper.hasRole(event.getMember(), Options.getEditRoleId()) && !DCHelper.hasRole(event.getMember(), Options.getCreateRoleId())) {
@@ -156,9 +133,9 @@ public final class HNSCommand extends ListenerAdapter {
 												.addEmbeds(UserEmbed.of(userModel, UserEmbed.Type.HNS_ALL)).queue();
 										event.reply(R.string("the_entry_change_was_successfully_requested")).queue();
 									} else event.reply(R.string("the_entry_could_not_be_updated")).queue();
-								} event.reply(R.string("an_impossible_error_occurred")).queue();
-							} event.reply(R.string("an_impossible_error_occurred")).queue();
-						} event.reply(R.string("an_impossible_error_occurred")).queue();
+								}
+							}
+						}
 					} else if (databaseAdapter.editHnsUser(hnsUserModel) == 0) {
 						event.reply(R.string("the_entry_could_not_be_updated")).queue();
 					} else event.reply(R.string("the_entry_was_successfully_updated"))
@@ -169,9 +146,7 @@ public final class HNSCommand extends ListenerAdapter {
 		} else event.reply(R.string("your_command_was_incomplete")).queue();
 	}
 
-	private static HNSUserModel.HNSUserModelBuilder getHnsBuilder(@NonNull SlashCommandInteractionEvent event, HNSUserModel hnsUserModel) {
-		HNSUserModel.HNSUserModelBuilder hnsBuilder = hnsUserModel.toBuilder();
-
+	private static HNSUserModel buildHnsModel(@NonNull final SlashCommandInteractionEvent event, @NonNull final HNSUserModel.HNSUserModelBuilder hnsBuilder) {
 		for (OptionMapping optionMapping : event.getOptions()) {
 			switch (optionMapping.getName()) {
 				case "rating" -> hnsBuilder.rating(optionMapping.getAsString());
@@ -185,14 +160,14 @@ public final class HNSCommand extends ListenerAdapter {
 				case "highest_rank" -> hnsBuilder.highestRank(optionMapping.getAsString());
 			}
 		}
-		return hnsBuilder;
+		return hnsBuilder.build();
 	}
 
 	private void hnsList(@NonNull final SlashCommandInteractionEvent event) {
-		if (event.getOption("page") instanceof OptionMapping pageMapping) {
-			if (databaseAdapter.getHnsUserList(pageMapping.getAsInt()) instanceof List<HNSUserModel> entries && !entries.isEmpty()) {
-				if (databaseAdapter.getHnsPages() < pageMapping.getAsInt() && pageMapping.getAsInt() >= 0) {
-					int page = pageMapping.getAsInt();
+		if (event.getOption("page") instanceof final OptionMapping pageMapping) {
+			if (databaseAdapter.getHnsUserList(pageMapping.getAsInt()) instanceof final List<HNSUserModel> entries && !entries.isEmpty()) {
+				final var page = pageMapping.getAsInt();
+				if (databaseAdapter.getHnsPages() < page && page >= 0) {
 					event.deferReply().queue(interactionHook -> interactionHook.editOriginalEmbeds(ListEmbed.createHnsList(databaseAdapter, entries, page))
 							.setComponents(ActionRow.of(
 									Button.primary(String.format("previous:hns:%s", page), R.string("previous_page")).withDisabled(page < 1 || !Features.dev),
