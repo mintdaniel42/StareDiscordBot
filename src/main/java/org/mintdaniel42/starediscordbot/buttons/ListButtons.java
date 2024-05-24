06b9a0cf8c7e1cdff030e80dcd4879b31fd7f06d
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-24 12:01:15 +0200

    Button ActionRows should be created inside ListButtons

--- src/main/java/org/mintdaniel42/starediscordbot/buttons/ListButtons.java
+++ src/main/java/org/mintdaniel42/starediscordbot/buttons/ListButtons.java
@@ -2,11 +2,11 @@ package org.mintdaniel42.starediscordbot.buttons;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.jetbrains.annotations.Contract;
 import org.mintdaniel42.starediscordbot.db.DatabaseAdapter;
 import org.mintdaniel42.starediscordbot.embeds.ListEmbed;
 import org.mintdaniel42.starediscordbot.utils.R;
@@ -20,26 +20,44 @@ public final class ListButtons extends ListenerAdapter {
         String[] buttonParts = event.getComponentId().split(":");
         if (buttonParts.length != 3) return;
 
-        MessageEmbed messageEmbed;
-        final int page = Integer.parseInt(buttonParts[2]) + (buttonParts[0].equals("previous") ? -1 : 1);
+        final var page = Integer.parseInt(buttonParts[2]) + (buttonParts[0].equals("previous") ? -1 : 1);
+        final var embedType = Type.valueOf(buttonParts[1]);
+        final var messageEmbed = switch (embedType) {
+            case pg -> ListEmbed.createPgList(databaseAdapter, page);
+            case hns -> ListEmbed.createHnsList(databaseAdapter, page);
+            default -> null;
+        };
+        final var actionRow = switch (embedType) {
+            case pg -> create(embedType, page, databaseAdapter.getPgPages());
+            case hns -> create(embedType, page, databaseAdapter.getHnsPages());
+            default -> null;
+        };
 
-        if (buttonParts[1].equals("pg")) {
-            messageEmbed = ListEmbed.createPgList(databaseAdapter, page);
-            event.deferReply().queue(interactionHook -> interactionHook.editOriginalEmbeds(messageEmbed).setComponents(
-                    ActionRow.of(
-                            Button.primary(String.format("previous:pg:%s", page), R.string("previous_page")).withDisabled(page < 1),
-                            Button.primary(String.format("next:pg:%s", page), R.string("next_page")).withDisabled(page + 1 >= databaseAdapter.getPgPages())
-                    )
-            ).queue());
-        }
-        else if (buttonParts[1].equals("hns")) {
-            messageEmbed = ListEmbed.createHnsList(databaseAdapter, page);
-            event.deferReply().queue(interactionHook -> interactionHook.editOriginalEmbeds(messageEmbed).setComponents(
-                    ActionRow.of(
-                            Button.primary(String.format("previous:hns:%s", page), R.string("previous_page")).withDisabled(page < 1),
-                            Button.primary(String.format("next:hns:%s", page), R.string("next_page")).withDisabled(page + 1 >= databaseAdapter.getHnsPages())
-                    )
-            ).queue());
-        }
+        if (messageEmbed != null) {
+            event.deferEdit().queue(interactionHook -> interactionHook.editOriginalEmbeds(messageEmbed)
+                    .setComponents(actionRow)
+                    .queue());
+        } else event.deferEdit().queue();
+    }
+
+    @Contract(pure = true, value = "_, _, _ -> new")
+    public static @NonNull ActionRow create(@NonNull final Type type, final int page, final long maxPages) {
+        return ActionRow.of(
+                Button.primary(
+                        "previous:%s:%s".formatted(type.name(), page),
+                        R.string("previous_page")
+                ).withDisabled(page <= 0),
+                Button.primary(
+                        "next:%s:%s".formatted(type.name(), page),
+                        R.string("next_page")
+                ).withDisabled(page >= maxPages)
+        );
+    }
+
+    public enum Type {
+        hns,
+        pg,
+        group,
+        help
     }
 }
