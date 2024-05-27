e8ab0c8bcfbc7d7176532a19fc3ceab605f24600
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-27 11:24:34 +0200

    Document R.java, deprecate old api & replace with new one

--- src/main/java/org/mintdaniel42/starediscordbot/utils/R.java
+++ src/main/java/org/mintdaniel42/starediscordbot/utils/R.java
@@ -7,15 +7,53 @@ import org.mintdaniel42.starediscordbot.build.BuildConfig;
 
 import java.util.ResourceBundle;
 
+/**
+ * A utility class for easier resource access
+ */
 @UtilityClass
 public class R {
-	public @NonNull String string(@NonNull @PropertyKey(resourceBundle = "strings") final String string, Object... args) {
-		ResourceBundle resourceBundle = ResourceBundle.getBundle("strings", BuildConfig.locale);
-		return resourceBundle.containsKey(string) ? resourceBundle.getString(string).formatted(args) : string;
+	@Deprecated
+	public @NonNull String string(@NonNull @PropertyKey(resourceBundle = "ui") final String string, Object... args) {
+		return Strings.ui(string, args);
 	}
 
-	public @NonNull String logging(@NonNull @PropertyKey(resourceBundle = "logging") final String string, Object... args) {
-		ResourceBundle resourceBundle = ResourceBundle.getBundle("logging", BuildConfig.locale);
-		return resourceBundle.containsKey(string) ? resourceBundle.getString(string).formatted(args) : string;
+	@Deprecated
+	public @NonNull String logging(@NonNull @PropertyKey(resourceBundle = "log") final String string, Object... args) {
+		return Strings.log(string, args);
+	}
+
+	/**
+	 * This utility class contains two methods:<br>
+	 * {@code Strings.ui(stringKey, (optional) arguments...)}<br>
+	 * {@code Strings.log(stringKey, (optional) arguments...)}<br>
+	 * Both of them do the same thing but for two different resource bundles.
+	 * The first one handles all strings the user can see in messages and commands
+	 * while the second one contains all strings that are used for logging.
+	 */
+	@UtilityClass
+	public class Strings {
+		/**
+		 * Get strings from the resource bundle {@code ui}
+		 *
+		 * @param string the string key in the resource bundle
+		 * @param args   if formatting qualifiers are present they have to be specified here
+		 * @return the string from the resourcebundle if it exists, else the string key
+		 */
+		public @NonNull String ui(@NonNull @PropertyKey(resourceBundle = "ui") final String string, Object... args) {
+			ResourceBundle resourceBundle = ResourceBundle.getBundle("ui", BuildConfig.locale);
+			return resourceBundle.containsKey(string) ? resourceBundle.getString(string).formatted(args) : string;
+		}
+
+		/**
+		 * Get strings from the resource bundle {@code log}
+		 *
+		 * @param string the string key in the resource bundle
+		 * @param args   if formatting qualifiers are present they have to be specified here
+		 * @return the string from the resourcebundle if it exists, else the string key
+		 */
+		public @NonNull String log(@NonNull @PropertyKey(resourceBundle = "log") final String string, Object... args) {
+			ResourceBundle resourceBundle = ResourceBundle.getBundle("log", BuildConfig.locale);
+			return resourceBundle.containsKey(string) ? resourceBundle.getString(string).formatted(args) : string;
+		}
 	}
 }
