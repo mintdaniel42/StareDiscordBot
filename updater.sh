334bd197f1598ccb8360b4906bfa12c944557f58
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-28 20:08:36 +0200

    Overwrite existing docker-compose file

--- updater.sh
+++ updater.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/mintdaniel42/StareDiscordBot/master/docker-compose.yml
+curl -O https://raw.githubusercontent.com/mintdaniel42/StareDiscordBot/master/docker-compose.yml
 
 if command -v docker-compose &> /dev/null; then
   docker-compose up -d
