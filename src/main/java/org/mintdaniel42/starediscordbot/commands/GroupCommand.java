aa2fd4c719fe044163fbe6edf3e2cdf540ee3b8d
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-29 12:53:44 +0200

    Fix missing group members

--- src/main/java/org/mintdaniel42/starediscordbot/commands/GroupCommand.java
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/GroupCommand.java
@@ -177,7 +177,7 @@ public final class GroupCommand extends ListenerAdapter {
 		if (event.getOption("username") instanceof OptionMapping usernameMapping) {
 			if (MCHelper.getUuid(databaseAdapter, usernameMapping.getAsString()) instanceof UUID uuid) {
 				if (databaseAdapter.getUser(uuid) instanceof UserModel userModel) {
-					if (userModel.getGroup() instanceof GroupModel groupModel) {
+					if (databaseAdapter.getGroup(userModel.getGroup().getTag()) instanceof GroupModel groupModel) {
 						event.replyEmbeds(GroupEmbed.of(databaseAdapter, groupModel, 0))
 								.setComponents(ListButtons.create(groupModel, 0, databaseAdapter.getGroupMemberPages(groupModel.getTag())))
 								.queue();
