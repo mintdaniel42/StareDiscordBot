3c87b0b9180df468736c5bc211cde185c035d697
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-05-29 11:48:35 +0200

    Make preparations for Group List Buttons

--- src/main/java/org/mintdaniel42/starediscordbot/buttons/ListButtons.java
+++ src/main/java/org/mintdaniel42/starediscordbot/buttons/ListButtons.java
@@ -8,6 +8,8 @@ import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.jetbrains.annotations.Contract;
 import org.mintdaniel42.starediscordbot.db.DatabaseAdapter;
+import org.mintdaniel42.starediscordbot.db.GroupModel;
+import org.mintdaniel42.starediscordbot.embeds.GroupEmbed;
 import org.mintdaniel42.starediscordbot.embeds.ListEmbed;
 import org.mintdaniel42.starediscordbot.utils.R;
 
@@ -15,29 +17,18 @@ import org.mintdaniel42.starediscordbot.utils.R;
 public final class ListButtons extends ListenerAdapter {
     @NonNull private final DatabaseAdapter databaseAdapter;
 
-    @Override
-    public void onButtonInteraction(@NonNull final ButtonInteractionEvent event) {
-        String[] buttonParts = event.getComponentId().split(":");
-        if (buttonParts.length != 3) return;
-
-        final var page = Integer.parseInt(buttonParts[2]) + (buttonParts[0].equals("previous") ? -1 : 1);
-        final var embedType = Type.valueOf(buttonParts[1]);
-        final var messageEmbed = switch (embedType) {
-            case pg -> ListEmbed.createPgList(databaseAdapter, page);
-            case hns -> ListEmbed.createHnsList(databaseAdapter, page);
-            default -> null;
-        };
-        final var actionRow = switch (embedType) {
-            case pg -> create(embedType, page, databaseAdapter.getPgPages());
-            case hns -> create(embedType, page, databaseAdapter.getHnsPages());
-            default -> null;
-        };
-
-        if (messageEmbed != null) {
-            event.deferEdit().queue(interactionHook -> interactionHook.editOriginalEmbeds(messageEmbed)
-                    .setComponents(actionRow)
-                    .queue());
-        } else event.deferEdit().queue();
+    @Contract(pure = true, value = "_, _, _ -> new")
+    public static @NonNull ActionRow create(@NonNull final GroupModel groupModel, final int page, final long maxPages) {
+        return ActionRow.of(
+                Button.primary(
+                        "previous:group:%s:%s".formatted(groupModel.getTag(), page),
+                        R.Strings.ui("previous_page")
+                ).withDisabled(page <= 0),
+                Button.primary(
+                        "next:group:%s:%s".formatted(groupModel.getTag(), page),
+                        R.Strings.ui("next_page")
+                ).withDisabled(page >= maxPages - 1)
+        );
     }
 
     @Contract(pure = true, value = "_, _, _ -> new")
@@ -52,6 +43,47 @@ public final class ListButtons extends ListenerAdapter {
 						R.Strings.ui("next_page")
                 ).withDisabled(page >= maxPages - 1)
         );
+    }
+
+    @Override
+    public void onButtonInteraction(@NonNull final ButtonInteractionEvent event) {
+        String[] buttonParts = event.getComponentId().split(":");
+        if (buttonParts.length == 3 || (buttonParts.length == 4 && buttonParts[1].equals("group"))) {
+            final int page;
+            if (buttonParts.length == 3)
+                page = Integer.parseInt(buttonParts[2]) + (buttonParts[0].equals("previous") ? -1 : 1);
+            else page = Integer.parseInt(buttonParts[3]) + (buttonParts[0].equals("previous") ? -1 : 1);
+            final var embedType = Type.valueOf(buttonParts[1]);
+            GroupModel groupModel = null;
+            final var messageEmbed = switch (embedType) {
+                case pg -> ListEmbed.createPgList(databaseAdapter, page);
+                case hns -> ListEmbed.createHnsList(databaseAdapter, page);
+                case group -> {
+                    if ((groupModel = databaseAdapter.getGroup(buttonParts[2])) != null) {
+                        yield GroupEmbed.of(databaseAdapter, groupModel, page);
+                    }
+                    yield null;
+                }
+                default -> null;
+            };
+            final var actionRow = switch (embedType) {
+                case pg -> create(embedType, page, databaseAdapter.getPgPages());
+                case hns -> create(embedType, page, databaseAdapter.getHnsPages());
+                case group -> {
+                    if (groupModel != null) {
+                        yield create(groupModel, page, databaseAdapter.getGroupMemberPages(groupModel.getTag()));
+                    }
+                    yield null;
+                }
+                default -> null;
+            };
+
+            if (messageEmbed != null) {
+                event.deferEdit().queue(interactionHook -> interactionHook.editOriginalEmbeds(messageEmbed)
+                        .setComponents(actionRow)
+                        .queue());
+            } else event.deferEdit().queue();
+        }
     }
 
     public enum Type {
