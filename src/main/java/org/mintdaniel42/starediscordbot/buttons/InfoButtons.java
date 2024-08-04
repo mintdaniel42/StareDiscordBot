e468e5e047829701156f6d8adecb176a4176d4f1
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-04 21:12:26 +0200

    Allow to directly list entries when using /info

---
+++ src/main/java/org/mintdaniel42/starediscordbot/buttons/InfoButtons.java
@@ -0,0 +1,26 @@
+package org.mintdaniel42.starediscordbot.buttons;
+
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.jetbrains.annotations.Contract;
+import org.mintdaniel42.starediscordbot.utils.R;
+
+@UtilityClass
+public class InfoButtons extends ListenerAdapter {
+	@Contract(pure = true, value = "-> new")
+	public @NonNull ActionRow create() {
+		return ActionRow.of(
+				Button.primary(
+						"info:%s:%s".formatted("hns", -1),
+						R.Strings.ui("list_hide_n_seek_entries")
+				),
+				Button.primary(
+						"info:%s:%s".formatted("pg", -1),
+						R.Strings.ui("list_partygames_entries")
+				)
+		);
+	}
+}
--- src/main/java/org/mintdaniel42/starediscordbot/commands/InfoCommand.java
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/InfoCommand.java
@@ -4,6 +4,7 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.mintdaniel42.starediscordbot.buttons.InfoButtons;
 import org.mintdaniel42.starediscordbot.db.DatabaseAdapter;
 import org.mintdaniel42.starediscordbot.embeds.InfoEmbed;
 import org.mintdaniel42.starediscordbot.utils.Options;
@@ -17,7 +18,9 @@ public final class InfoCommand extends ListenerAdapter {
 	public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
 		if (event.getFullCommandName().equals("info")) {
 			if (!Options.isInMaintenance()) {
-				event.replyEmbeds(InfoEmbed.create(databaseAdapter)).queue();
+				event.replyEmbeds(InfoEmbed.create(databaseAdapter))
+						.addComponents(InfoButtons.create())
+						.queue();
 			} else event.reply(R.Strings.ui("the_bot_is_currently_in_maintenance_mode")).queue();
 		}
 	}
