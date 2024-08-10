a0c97ce4f09053a67d25665b90a1cf94ec0c604a
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-10 14:04:39 +0200

    Make TutorialButtons work
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

--- src/main/java/org/mintdaniel42/starediscordbot/buttons/TutorialButton.java
+++ src/main/java/org/mintdaniel42/starediscordbot/buttons/TutorialButton.java
@@ -1,25 +1,41 @@
 package org.mintdaniel42.starediscordbot.buttons;
 
 import lombok.NonNull;
-import lombok.experimental.UtilityClass;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.ItemComponent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.jetbrains.annotations.Nullable;
 import org.mintdaniel42.starediscordbot.data.TutorialModel;
+import org.mintdaniel42.starediscordbot.embeds.TutorialEmbed;
+import org.mintdaniel42.starediscordbot.utils.Options;
 import org.mintdaniel42.starediscordbot.utils.R;
 
 import java.util.Arrays;
 
-@UtilityClass
 public class TutorialButton extends ListenerAdapter {
-	public @Nullable ActionRow create(@NonNull final TutorialModel tutorialModel) {
+	public static @Nullable ActionRow create(@NonNull final TutorialModel tutorialModel) {
 		if (tutorialModel.getSimilar().length > 0) {
 			return ActionRow.of(Arrays.stream(tutorialModel.getSimilar())
-					.map(s -> Button.primary(s, R.Tutorials.get(s).getTitle()))
+					.map(s -> Button.primary("tutorial:" + s, R.Tutorials.get(s).getTitle()))
 					.toArray(ItemComponent[]::new));
 		}
 		return null;
+	}
+
+	@Override
+	public void onButtonInteraction(@NonNull final ButtonInteractionEvent event) {
+		String[] buttonParts = event.getComponentId().split(":");
+		if (buttonParts[0].equals("tutorial") && buttonParts.length == 2) {
+			if (!Options.isInMaintenance()) {
+				if (R.Tutorials.get(buttonParts[1]) instanceof TutorialModel tutorialModel) {
+					final var actionBar = TutorialButton.create(tutorialModel);
+					final var callback = event.replyEmbeds(TutorialEmbed.of(tutorialModel));
+					if (actionBar != null) callback.addComponents(actionBar);
+					callback.queue();
+				} else event.reply(R.Strings.ui("this_page_does_not_exist")).queue();
+			} else event.reply(R.Strings.ui("the_bot_is_currently_in_maintenance_mode")).queue();
+		}
 	}
 }
