2b4176341f9dae30698848e8ddbc642ea5d9bcf2
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-10 13:37:18 +0200

    Add the component bar

--- src/main/java/org/mintdaniel42/starediscordbot/commands/TutorialCommand.java
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/TutorialCommand.java
@@ -4,6 +4,7 @@ import lombok.NonNull;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.mintdaniel42.starediscordbot.buttons.TutorialButton;
 import org.mintdaniel42.starediscordbot.data.TutorialModel;
 import org.mintdaniel42.starediscordbot.embeds.TutorialEmbed;
 import org.mintdaniel42.starediscordbot.utils.Options;
@@ -16,7 +17,9 @@ public final class TutorialCommand extends ListenerAdapter {
 			if (!Options.isInMaintenance()) {
 				if (event.getOption("page") instanceof OptionMapping pageMapping) {
 					if (R.Tutorials.get(pageMapping.getAsString()) instanceof TutorialModel tutorialModel) {
-						event.replyEmbeds(TutorialEmbed.of(tutorialModel)).queue();
+						event.replyEmbeds(TutorialEmbed.of(tutorialModel))
+								.addComponents(TutorialButton.create(tutorialModel))
+								.queue();
 					} else event.reply(R.Strings.ui("this_page_does_not_exist")).queue();
 				} else event.reply(R.Strings.ui("your_command_was_incomplete")).queue();
 			} else event.reply(R.Strings.ui("the_bot_is_currently_in_maintenance_mode")).queue();
