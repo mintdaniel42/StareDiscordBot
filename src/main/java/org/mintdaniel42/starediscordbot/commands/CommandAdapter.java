cc8ec3271d26bc57ae722c4398d1769c41315924
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-13 10:32:27 +0200

    Document CommandAdapter
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

--- src/main/java/org/mintdaniel42/starediscordbot/commands/CommandAdapter.java
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/CommandAdapter.java
@@ -4,10 +4,20 @@ import lombok.NonNull;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.requests.restaction.WebhookMessageEditAction;
 
+/**
+ * Implementing this interface is equivalent to implement the logic for a command.
+ * It has to be registered in {@link CommandDispatcher}
+ */
+@FunctionalInterface
 public interface CommandAdapter {
-	// TODO DOCS
+	/**
+	 * @param interactionHook this is the {@link InteractionHook} which has to be used for responses
+	 * @param event           contains read-only data like {@link OptionMapping}, should not be used for responding
+	 * @return the {@link WebhookMessageEditAction} returned by calling an interactionHook edit action
+	 */
 	@NonNull
 	WebhookMessageEditAction<Message> handle(@NonNull final InteractionHook interactionHook, @NonNull final SlashCommandInteractionEvent event);
 }
