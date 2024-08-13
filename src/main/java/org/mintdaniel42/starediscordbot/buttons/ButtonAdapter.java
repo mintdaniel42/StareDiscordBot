b61a3c60b85ef1f052c52cbeda26f6d3a4ca1d1d
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-13 10:34:54 +0200

    Document ButtonAdapter
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

--- src/main/java/org/mintdaniel42/starediscordbot/buttons/ButtonAdapter.java
+++ src/main/java/org/mintdaniel42/starediscordbot/buttons/ButtonAdapter.java
@@ -6,8 +6,17 @@ import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.requests.restaction.WebhookMessageEditAction;
 
+/**
+ * Implementing this interface is equivalent to implementing the logic for a button.
+ * It has to be registered in {@link ButtonDispatcher}
+ */
+@FunctionalInterface
 public interface ButtonAdapter {
-	// TODO DOCS
+	/**
+	 * @param interactionHook this is the {@link InteractionHook} which has to be used for responses
+	 * @param event           contains read-only data like the component id, should not be used for responding
+	 * @return the {@link WebhookMessageEditAction} returned by calling an interactionHook edit action
+	 */
 	@NonNull
 	WebhookMessageEditAction<Message> handle(@NonNull final InteractionHook interactionHook, @NonNull final ButtonInteractionEvent event);
 }
