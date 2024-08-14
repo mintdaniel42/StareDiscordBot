4673696c742b9625174864ce00557b4435eb42ea
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-14 10:26:13 +0200

    Add AchievementAddCommand
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

---
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/hns/achievements/AchievementAddCommand.java
@@ -0,0 +1,41 @@
+package org.mintdaniel42.starediscordbot.commands.hns.achievements;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageEditAction;
+import org.mintdaniel42.starediscordbot.commands.CommandAdapter;
+import org.mintdaniel42.starediscordbot.data.AchievementModel;
+import org.mintdaniel42.starediscordbot.data.DatabaseAdapter;
+import org.mintdaniel42.starediscordbot.embeds.AchievementEmbed;
+import org.mintdaniel42.starediscordbot.utils.R;
+
+@RequiredArgsConstructor
+public class AchievementAddCommand implements CommandAdapter {
+	@NonNull private final DatabaseAdapter databaseAdapter;
+
+	@Override
+	public @NonNull WebhookMessageEditAction<Message> handle(@NonNull final InteractionHook interactionHook, @NonNull final SlashCommandInteractionEvent event) {
+		if (event.getOption("name") instanceof final OptionMapping nameMapping &&
+				event.getOption("description") instanceof final OptionMapping descriptionMapping &&
+				event.getOption("type") instanceof final OptionMapping typeMapping &&
+				event.getOption("points") instanceof final OptionMapping pointsMapping) {
+
+			final var model = AchievementModel.builder()
+					.name(nameMapping.getAsString())
+					.description(descriptionMapping.getAsString())
+					.type(AchievementModel.Type.valueOf(typeMapping.getAsString()))
+					.points(pointsMapping.getAsInt())
+					.build();
+
+			return interactionHook.editOriginal(switch (databaseAdapter.addAchievement(model)) {
+				case SUCCESS -> R.Strings.ui("the_achievement_was_successfully_created");
+				case DUPLICATE -> R.Strings.ui("this_achievement_already_exists");
+				case ERROR -> R.Strings.ui("the_achievement_could_not_be_added");
+			}).setEmbeds(AchievementEmbed.of(model));
+		} else return interactionHook.editOriginal(R.Strings.ui("your_command_was_incomplete"));
+	}
+}
