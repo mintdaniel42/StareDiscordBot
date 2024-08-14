b647dcb1d50be74e02ad31f8d23fbd6b1f3670a3
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-14 19:25:50 +0200

    Add MapsAddCommand stub
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

---
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/hns/maps/MapsAddCommand.java
@@ -0,0 +1,28 @@
+package org.mintdaniel42.starediscordbot.commands.hns.maps;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageEditAction;
+import org.mintdaniel42.starediscordbot.commands.CommandAdapter;
+import org.mintdaniel42.starediscordbot.data.DatabaseAdapter;
+import org.mintdaniel42.starediscordbot.utils.R;
+
+@RequiredArgsConstructor
+public final class MapsAddCommand implements CommandAdapter {
+	@NonNull private final DatabaseAdapter databaseAdapter;
+
+	@Override
+	public @NonNull WebhookMessageEditAction<Message> handle(@NonNull final InteractionHook interactionHook, @NonNull final SlashCommandInteractionEvent event) {
+		if (event.getOption("name") instanceof final OptionMapping nameMapping &&
+				event.getOption("builder") instanceof final OptionMapping builderMapping &&
+				event.getOption("release") instanceof final OptionMapping releaseMapping &&
+				event.getOption("picture") instanceof final OptionMapping pictureMapping &&
+				event.getOption("difficulty") instanceof final OptionMapping difficultyMapping) {
+			return null;
+		} else return interactionHook.editOriginal(R.Strings.ui("your_command_was_incomplete"));
+	}
+}
