4c46a3e0ed4e4f96a91eecbc3ca70d787b27e0b5
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-14 11:31:49 +0200

    First working version of AchievementsListCommand
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

---
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/hns/achievements/AchievementsListCommand.java
@@ -0,0 +1,42 @@
+package org.mintdaniel42.starediscordbot.commands.hns.achievements;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageEditAction;
+import org.mintdaniel42.starediscordbot.commands.CommandAdapter;
+import org.mintdaniel42.starediscordbot.data.AchievementModel;
+import org.mintdaniel42.starediscordbot.data.DatabaseAdapter;
+import org.mintdaniel42.starediscordbot.embeds.AchievementEmbed;
+import org.mintdaniel42.starediscordbot.utils.R;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class AchievementsListCommand implements CommandAdapter {
+	@NonNull private final DatabaseAdapter databaseAdapter;
+
+	@Override
+	public @NonNull WebhookMessageEditAction<Message> handle(@NonNull final InteractionHook interactionHook, @NonNull final SlashCommandInteractionEvent event) {
+		final AchievementModel.Type type;
+		final int points;
+		final int page;
+		if (event.getOption("type") instanceof final OptionMapping typeMapping) {
+			type = AchievementModel.Type.valueOf(typeMapping.getAsString());
+		} else type = null;
+		if (event.getOption("points") instanceof final OptionMapping pointsMapping) {
+			points = pointsMapping.getAsInt();
+		} else points = -1;
+		if (event.getOption("page") instanceof final OptionMapping pageMapping) {
+			page = pageMapping.getAsInt();
+		} else page = 1;
+		if (databaseAdapter.getAchievements(type, points) instanceof final List<AchievementModel> achievementModels) {
+			if (page < achievementModels.size()) {
+				return interactionHook.editOriginalEmbeds(AchievementEmbed.of(achievementModels.get(page - 1)));
+			} else return interactionHook.editOriginal(R.Strings.ui("this_page_does_not_exist"));
+		} else return interactionHook.editOriginal(R.Strings.ui("no_entries_available"));
+	}
+}
