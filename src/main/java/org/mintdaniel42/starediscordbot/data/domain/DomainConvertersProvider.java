c407a3ff2c9acd586c74d513d5da0035bd42efd3
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-16 12:11:55 +0200

    Add UUID converter provider
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

---
+++ src/main/java/org/mintdaniel42/starediscordbot/data/domain/DomainConvertersProvider.java
@@ -0,0 +1,6 @@
+package org.mintdaniel42.starediscordbot.data.domain;
+
+import org.seasar.doma.DomainConverters;
+
+@DomainConverters({UUIDConverter.class})
+public final class DomainConvertersProvider {}
