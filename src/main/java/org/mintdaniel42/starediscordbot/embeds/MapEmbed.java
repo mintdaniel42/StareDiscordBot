fc22bb634d0dda1223be1025a89cdd6a95f3b5e4
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-16 14:12:53 +0200

    Rename MapEntity because of merge
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

--- src/main/java/org/mintdaniel42/starediscordbot/embeds/MapEmbed.java
+++ src/main/java/org/mintdaniel42/starediscordbot/embeds/MapEmbed.java
@@ -5,18 +5,18 @@ import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.jetbrains.annotations.Contract;
-import org.mintdaniel42.starediscordbot.data.MapModel;
+import org.mintdaniel42.starediscordbot.data.entity.MapEntity;
 import org.mintdaniel42.starediscordbot.utils.R;
 
 @UtilityClass
 public class MapEmbed {
 	@Contract(pure = true, value = "_ -> new")
-	public @NonNull MessageEmbed of(@NonNull final MapModel mapModel) {
+	public @NonNull MessageEmbed of(@NonNull final MapEntity map) {
 		return new EmbedBuilder()
 				.setTitle(R.Strings.ui("map_details"))
-				.addField(R.Strings.ui("the_maps_name"), mapModel.getName(), false)
-				.addField(R.Strings.ui("the_maps_release_date"), mapModel.getRelease().toString(), false) // TODO: format date
-				.addField(R.Strings.ui("the_maps_difficulty"), mapModel.getDifficulty().toString(), false) // TODO: use strings for difficulty text
+				.addField(R.Strings.ui("the_maps_name"), map.getName(), false)
+				.addField(R.Strings.ui("the_maps_release_date"), map.getRelease().toString(), false) // TODO: format date
+				.addField(R.Strings.ui("the_maps_difficulty"), map.getDifficulty().toString(), false) // TODO: use strings for difficulty text
 				.build();
 	}
 }
