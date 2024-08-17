2db80db7d5fb06524d0ed9f585ba74603ac2cec4
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-17 12:02:12 +0200

    Add PoolAdapter & RateLimited interfaces for token-bucket rate limiting
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

---
+++ src/main/java/org/mintdaniel42/starediscordbot/bucket/RateLimited.java
@@ -0,0 +1,24 @@
+package org.mintdaniel42.starediscordbot.bucket;
+
+import lombok.NonNull;
+
+/**
+ * This interface sets up anything related to rate limiting
+ */
+public interface RateLimited {
+	/**
+	 * @return the wanted PoolAdapter (usually {@link DefaultBucketPool})
+	 */
+	default @NonNull PoolAdapter getPool() {
+		return DefaultBucketPool.getInstance();
+	}
+
+	/**
+	 * Override this to change how many tokens an action requires
+	 *
+	 * @return the amount of tokens
+	 */
+	default long getActionTokenPrice() {
+		return 1;
+	}
+}
