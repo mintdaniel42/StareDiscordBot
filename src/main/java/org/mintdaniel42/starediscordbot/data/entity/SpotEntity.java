c397fe944bbbcff3deb683f7ce487d0846a131af
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-17 13:37:58 +0200

    Add SpotEntity
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

---
+++ src/main/java/org/mintdaniel42/starediscordbot/data/entity/SpotEntity.java
@@ -0,0 +1,31 @@
+package org.mintdaniel42.starediscordbot.data.entity;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+import org.seasar.doma.Entity;
+import org.seasar.doma.Id;
+import org.seasar.doma.Metamodel;
+import org.seasar.doma.Table;
+
+import java.util.UUID;
+
+@Builder(toBuilder = true)
+@Entity(immutable = true, metamodel = @Metamodel(suffix = "Meta"))
+@Table(name = "spots")
+@Value
+public class SpotEntity {
+	@NonNull @Id UUID uuid;
+	@NonNull UUID mapUUID;
+	UUID finder;
+	@NonNull String blockId;
+	@NonNull String rating;
+	@NonNull String videolink;
+	@NonNull Type type;
+	boolean twoPlayers;
+
+	public enum Type {
+		afk,
+		spelling
+	}
+}
