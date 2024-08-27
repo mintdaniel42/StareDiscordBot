712db3fbb17ef07dce75cdfd0b660fa555c1faa2
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-08-27 21:26:48 +0200

    Allow retrieving of Entities from the repository via composer
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

--- src/main/java/org/mintdaniel42/starediscordbot/compose/ComposerHelper.java
+++ src/main/java/org/mintdaniel42/starediscordbot/compose/ComposerHelper.java
@@ -2,7 +2,9 @@ package org.mintdaniel42.starediscordbot.compose;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.mintdaniel42.starediscordbot.compose.exceptions.NoSuchEntryException;
 import org.mintdaniel42.starediscordbot.compose.exceptions.UnknownUsernameException;
+import org.mintdaniel42.starediscordbot.data.repository.BaseRepository;
 import org.mintdaniel42.starediscordbot.data.repository.UsernameRepository;
 import org.mintdaniel42.starediscordbot.utils.MCHelper;
 
@@ -16,12 +18,25 @@ public abstract class ComposerHelper {
 
 	/* ========== UUID ========== */
 	protected final @NonNull UUID requireUUID(@NonNull final String username) {
-		return requireUUID(username, uuid -> uuid);
+		return Optional.ofNullable(MCHelper.getUuid(usernameRepository, username))
+				.orElseThrow(UnknownUsernameException::new);
 	}
 
 	protected final @NonNull <T> T requireUUID(@NonNull final String username, @NonNull final Function<UUID, T> function) {
 		return Optional.ofNullable(MCHelper.getUuid(usernameRepository, username))
 				.map(function)
 				.orElseThrow(UnknownUsernameException::new);
+	}
+
+	/* ========== ENTITIES ========== */
+	protected final @NonNull <ID, ENTITY> ENTITY requireEntity(@NonNull final BaseRepository<ID, ENTITY> repository, ID id) {
+		return repository.selectById(id)
+				.orElseThrow(NoSuchEntryException::new);
+	}
+
+	protected final @NonNull <ID, ENTITY, T> T requireEntity(@NonNull final BaseRepository<ID, ENTITY> repository, ID id, Function<ENTITY, T> function) {
+		return repository.selectById(id)
+				.map(function)
+				.orElseThrow(NoSuchEntryException::new);
 	}
 }
