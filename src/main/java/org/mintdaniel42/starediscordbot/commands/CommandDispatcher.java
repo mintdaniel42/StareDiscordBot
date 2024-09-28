595381a70713ec9af6174837b2d38e8973cc5c4d
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-09-28 10:00:37 +0200

    Rearrange and optimize code
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

--- src/main/java/org/mintdaniel42/starediscordbot/commands/CommandDispatcher.java
+++ src/main/java/org/mintdaniel42/starediscordbot/commands/CommandDispatcher.java
@@ -4,15 +4,20 @@ import jakarta.inject.Singleton;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+import net.dv8tion.jda.api.utils.messages.MessageEditData;
 import org.jetbrains.annotations.Contract;
 import org.mintdaniel42.starediscordbot.BotConfig;
+import org.mintdaniel42.starediscordbot.build.BuildConfig;
 import org.mintdaniel42.starediscordbot.commands.misc.MaintenanceCommand;
+import org.mintdaniel42.starediscordbot.compose.command.BaseComposeCommand;
+import org.mintdaniel42.starediscordbot.compose.command.CommandContext;
 import org.mintdaniel42.starediscordbot.embeds.ErrorEmbed;
+import org.mintdaniel42.starediscordbot.exception.BotException;
 import org.mintdaniel42.starediscordbot.utils.R;
 
 import java.time.Duration;
@@ -29,39 +34,16 @@ public final class CommandDispatcher extends ListenerAdapter {
 
 	@Override
 	public void onSlashCommandInteraction(@NonNull final SlashCommandInteractionEvent event) {
-		//#if dev
-		if (event.getMember() instanceof final Member member) {
+		if (!BuildConfig.production && event.getMember() instanceof final Member member) {
 			log.info(R.Strings.log("command_s_invoked_by_user_s",
 					event.getFullCommandName(),
 					member.getEffectiveName()));
 		}
-		//#endif
 
 		dispatch(event).ifPresentOrElse(adapter ->
 						event.deferReply()
 								.setEphemeral(adapter.isPublicResponseRestricted())
-								.queue(interactionHook -> {
-									try {
-										if (adapter instanceof MaintenanceCommand || adapter.getPool()
-												.getBucket(Objects.requireNonNull(event.getMember()))
-												.asBlocking()
-												.tryConsume(adapter.getActionTokenPrice(), Duration.ofSeconds(10))) {
-											interactionHook.editOriginal(adapter.handle(event)).queue();
-										} else
-											interactionHook.editOriginal(R.Strings.ui("you_dont_have_enough_tokens_for_this_action_please_wait_a_few_seconds")).queue();
-									} catch (Exception e) {
-										if (event.getGuild() instanceof final Guild guild) {
-											if (guild.getTextChannelById(config.getLogChannelId()) instanceof final TextChannel channel) {
-												channel.sendMessageEmbeds(ErrorEmbed.of(event.getInteraction(), e)).queue();
-											}
-										}
-										interactionHook.editOriginal(R.Strings.ui("an_error_occurred_the_developer_has_been_notified"))
-												.queue();
-										//#if dev
-										//$$ throw new RuntimeException(e);
-										//#endif
-									}
-								})
+								.queue(interactionHook -> respond(interactionHook, adapter, event))
 				, () -> event.reply((R.Strings.ui(config.isInMaintenance() ? "the_bot_is_currently_in_maintenance_mode" : "you_do_not_have_the_permission_to_use_this_command")))
 						.setEphemeral(true)
 						.queue());
@@ -73,5 +55,38 @@ public final class CommandDispatcher extends ListenerAdapter {
 				.filter(commandAdapter -> commandAdapter.getCommandId().equals(event.getFullCommandName()))
 				.filter(commandAdapter -> commandAdapter.hasPermission(event.getMember()))
 				.findFirst();
+	}
+
+	private void respond(@NonNull final InteractionHook interactionHook, @NonNull final CommandAdapter adapter, @NonNull final SlashCommandInteractionEvent event) {
+		try {
+			if (adapter instanceof MaintenanceCommand || adapter.getPool()
+					.getBucket(Objects.requireNonNull(event.getMember()))
+					.asBlocking()
+					.tryConsume(adapter.getActionTokenPrice(), Duration.ofSeconds(10))) {
+				interactionHook.editOriginal(adapter.handle(event)).queue();
+			} else
+				interactionHook.editOriginal(R.Strings.ui("you_dont_have_enough_tokens_for_this_action_please_wait_a_few_seconds")).queue();
+		} catch (Exception e) {
+			interactionHook.editOriginal(new ExceptionHandler(e, event.getInteraction()).handle(event)).queue();
+			if (!BuildConfig.production) throw new RuntimeException(e);
+		}
+	}
+
+	@RequiredArgsConstructor
+	class ExceptionHandler extends BaseComposeCommand {
+		@NonNull private final Exception exception;
+		@NonNull private final SlashCommandInteraction interaction;
+
+		@Override
+		protected @NonNull MessageEditData compose(@NonNull final CommandContext context) throws BotException {
+			final var logChannel = requireChannel(context, config.getGuildId(), config.getLogChannelId());
+			logChannel.sendMessageEmbeds(ErrorEmbed.of(interaction, exception)).queue();
+			return response("an_error_occurred_the_developer_has_been_notified");
+		}
+
+		@Override
+		public @NonNull String getCommandId() {
+			return "";
+		}
 	}
 }
