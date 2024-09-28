41be4ba560fbcedb6d5356b42444824df0278265
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-09-28 10:39:25 +0200

    Remove more deprecated stuff from Options
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

--- src/main/java/org/mintdaniel42/starediscordbot/utils/Options.java
+++ src/main/java/org/mintdaniel42/starediscordbot/utils/Options.java
@@ -2,7 +2,6 @@ package org.mintdaniel42.starediscordbot.utils;
 
 import io.avaje.config.Config;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.UtilityClass;
 import org.mintdaniel42.starediscordbot.build.BuildConfig;
 
@@ -14,21 +13,6 @@ import org.mintdaniel42.starediscordbot.build.BuildConfig;
 @Deprecated(forRemoval = BuildConfig.production)
 public class Options {
     /**
-     * The ID of the role which allows to edit entries of groups & users
-     */
-    @Getter private final long p2Id = Config.getLong("p2.role.id");
-
-    /**
-     * Additional role (currently) only used for spot token-bucket mechanism
-     */
-    @Getter private final long p3Id = Config.getLong("p3.role.id");
-
-    /**
-     * The ID of the role which allows to create & edit entries of groups & users
-     */
-    @Getter private final long p4Id = Config.getLong("p4.role.id");
-
-    /**
      * The color all embeds should have
      */
     @Getter private final int colorNormal = Integer.parseInt(Config.get("color.normal"), 16);
@@ -37,9 +21,4 @@ public class Options {
      * The color the request embeds have
      */
     @Getter private final int colorRequest = Integer.parseInt(Config.get("color.request"), 16);
-
-    /**
-     * The Bot maintenance flag
-     */
-    @Getter @Setter private boolean inMaintenance = false;
 }
