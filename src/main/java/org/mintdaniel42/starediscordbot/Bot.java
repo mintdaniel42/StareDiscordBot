44bccd74197f3dc4adde5e0109d2cf0fba504439
mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>
2024-10-04 18:59:00 +0200

    Remove JCP remains
    Signed-off-by: mintdaniel42 <59701285+mintdaniel42@users.noreply.github.com>

--- src/main/java/org/mintdaniel42/starediscordbot/Bot.java
+++ src/main/java/org/mintdaniel42/starediscordbot/Bot.java
@@ -1,6 +1,5 @@
 package org.mintdaniel42.starediscordbot;
 
-import com.codahale.metrics.MetricRegistry;
 import jakarta.inject.Singleton;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -9,12 +8,11 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import org.mintdaniel42.starediscordbot.build.BuildConfig;
 import org.mintdaniel42.starediscordbot.data.Database;
 import org.mintdaniel42.starediscordbot.di.DI;
 import org.mintdaniel42.starediscordbot.exception.BotException;
 import org.mintdaniel42.starediscordbot.utils.R;
-
-import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Singleton
@@ -23,12 +21,9 @@ public final class Bot {
 	@NonNull private final Database database;
 	//@NonNull private final Scheduler scheduler;
 	@NonNull private final BotConfig config;
-	@NonNull private final MetricRegistry metrics;
 
-	public static void main(String[] args) throws BotException {
-		//#if dev
-		log.info(R.Strings.log("running_in_dev_mode"));
-		//#endif
+	public static void main(@NonNull final String... args) throws BotException {
+		if (!BuildConfig.production) log.info(R.Strings.log("running_in_dev_mode"));
 		DI.get(Bot.class).run();
 	}
 
